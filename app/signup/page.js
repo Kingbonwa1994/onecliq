90e7ee2c734cd4368f8034cee364017baaf652af
Kingbonwa1994 <bonwamdluli@gmail.com>
2023-09-08 14:59:17 +0200

    "redesigning the ui"

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,24 +2,7 @@ import SignupForm from '@/components/auth/Signup'
 
 
 export default function signUpPage() {
-  async function createAccount(data) {
 
-    const db = await mongoose.connect(MONGODB_URL);
-    const collection = db.collection("servicesProviders");
-    const serviceProvider = await collection.findOne({ email: data.email });
-    if (serviceProvider) {
-      throw new Error("Email already exists");
-    }
-  
-    const services = data.services.slice(0, 3);
-    const newServiceProvider = {
-      email: data.email,
-      phone: data.phone,
-      password: data.password,
-      services,
-    }; 
-    }
-  
   return (
     
     <div>
