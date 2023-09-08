055a25b1fe479e48ce85a546c08a4cb10eea90ea
Kingbonwa1994 <bonwamdluli@gmail.com>
2023-09-08 18:09:08 +0200

    "mongo db setup"

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,6 +1,10 @@
+"use client"
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 
 export default function SignUpPage() {
 
@@ -12,12 +16,26 @@ export default function SignUpPage() {
     
   })
 
+  const [loading, setLoading] = useState(false)
+
+  const onSignup = async() => {
+    try {
+      setLoading(true)
+      const response= await axios.post("/api/serviceProviders/signup", serviceProvider)
+      router.push("/login")
+    } catch (error) {
+      console.log("Signup failed", error.message)
+      toast.error(error.message)
+    }finally{
+      setLoading(false)
+    }
+   }
 
   return (
     <div className="bg-gray-900 min-h-screen flex justify-center items-center">
       <div className="bg-gray-800 p-8 rounded shadow-lg w-96">
         <h2 className="text-2xl font-semibold mb-4 text-white">
-          Sign Up as a Service Provider
+          {loading ? "Processing" : "Signup"}
         </h2>
         <form>
           <div className="mb-4">
@@ -108,10 +126,11 @@ export default function SignUpPage() {
           </div>
           <div className="mb-4">
             <button
+              onClick={onSignup}
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full"
               type="submit"
             >
-              Sign Up
+            Sign up
             </button>
           </div>
           <p className="text-gray-300 text-center">
